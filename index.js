c88ed3f101fcfb90fdd3d68861260985cbe50086
Tamojit Das <tamojitdas181007@gmail.com>
2024-10-23 16:52:52 +0530

    App init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+})
