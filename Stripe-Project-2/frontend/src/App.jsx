f937bc38a6111445a5ce8df2a6dd602a68c7dab0
Tamojit Das <tamojitdas181007@gmail.com>
2024-10-23 20:12:02 +0530

    Stripe integration with Node & React

--- Stripe-Project-2/frontend/src/App.jsx
+++ Stripe-Project-2/frontend/src/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState({
+    name: "FulJhuri",
+    price: 100,
+    productBy: "FB"
+  });
+
+  const makePayment = (token) => {
+    const body = {
+      token,
+      product
+    }
+
+    const headers = {
+      "Content-Type": "application/json"
+    }
+
+    return fetch(`http://localhost:8282/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    }).then(response => {
+      console.log("Response", response);
+      const { status } = response;
+      console.log("Status", status);
+    })
+      .catch(err => console.log(err))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StripeCheckout
+        stripeKey={import.meta.env.VITE_APP_STRIPE_KEY}
+        token={makePayment}
+        name="Buy"
+        amount={product.price * 100}
+      >
+        <button className="btn-large cyan">Buy Now!</button>
+      </StripeCheckout>
     </>
   )
 }
